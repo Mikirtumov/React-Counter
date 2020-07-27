95ad9dac661198d9050fe6d4c3480bf57ffc584d
Sergo Mikirtumov <mikirtumov@gmail.com>
2020-07-27 01:13:59 -0700

    Updates

---
+++ precache-manifest.47e870f8b91b478940d13e98f2055f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8521418e50defc2598befdaa420bb7a2",
+    "url": "/React-Counter/index.html"
+  },
+  {
+    "revision": "f094e3880328036834ac",
+    "url": "/React-Counter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f25d50c4cdf7fd60c268",
+    "url": "/React-Counter/static/js/2.d676fc10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Counter/static/js/2.d676fc10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f094e3880328036834ac",
+    "url": "/React-Counter/static/js/main.52069039.chunk.js"
+  },
+  {
+    "revision": "6fd47eed6cae08b69e2d",
+    "url": "/React-Counter/static/js/runtime-main.42f4a18f.js"
+  }
+]);
