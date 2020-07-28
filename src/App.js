2e3624b28ebe230663dc42ef8bbf0b50b9b334af
Sergo Mikirtumov <mikirtumov@gmail.com>
2020-07-28 14:50:51 -0700

    added 4 buttons to counter

--- src/App.js
+++ src/App.js
@@ -6,12 +6,24 @@ function App() {
     const [counters, setCounters] = useState([10,3,22]);
 
     const addCounters = () => {
-        setCounters([...counters, 5])
+        const r = Math.round(Math.random() * 10);
+        setCounters([...counters, r])
     }
+    const plusMinus = (n, value) => {
+        const newArr = [...counters];
+        newArr[n] += value
+        setCounters(newArr);
+    }
+
   return (
     <div className="App">
 
-        {counters.map((el, i)=> <Counter count={el} key={i}/>)}
+        {counters.map((el, i)=> <Counter
+            count={el}
+            key={i}
+            index={i}
+            plusMinus={plusMinus}
+            />)}
         <button onClick={addCounters}>Add Counter</button>
     </div>
   );
--- src/Counter.js
+++ src/Counter.js
@@ -4,7 +4,13 @@ import './App.css';
 function Counter(props) {
   return (
     <div className="App">
-        Counter {props.count}
+      <button onClick={() => props.plusMinus(props.index, -1)}>-1</button>
+      <button onClick={() => props.plusMinus(props.index, -2)}>-2</button>
+      <button onClick={() => props.plusMinus(props.index, -3)}>-3</button>
+      {props.count}
+      <button onClick={() => props.plusMinus(props.index, 3)}>+3</button>
+      <button onClick={() => props.plusMinus(props.index, 2)}>+2</button>
+      <button onClick={() => props.plusMinus(props.index, 1)}>+1</button>
 
     </div>
   );
