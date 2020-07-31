07eed9038a7a6e6c080e5cf6d0b6613fff00eeaf
Sergo Mikirtumov <mikirtumov@gmail.com>
2020-07-31 05:48:38 -0700

    button style Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -4,13 +4,13 @@ import './App.css';
 function Counter(props) {
   return (
     <div className="App">
-      <button onClick={() => props.plusMinus(props.index, -1)}>-1</button>
-      <button onClick={() => props.plusMinus(props.index, -2)}>-2</button>
-      <button onClick={() => props.plusMinus(props.index, -3)}>-3</button>
+      <button type="button" className="btn btn-outline-danger" onClick={() => props.plusMinus(props.index, -1)}>-1</button>
+      <button type="button" className="btn btn-outline-danger" onClick={() => props.plusMinus(props.index, -2)}>-2</button>
+      <button type="button" className="btn btn-outline-danger" onClick={() => props.plusMinus(props.index, -3)}>-3</button>
       {props.count}
-      <button onClick={() => props.plusMinus(props.index, 3)}>+3</button>
-      <button onClick={() => props.plusMinus(props.index, 2)}>+2</button>
-      <button onClick={() => props.plusMinus(props.index, 1)}>+1</button>
+      <button type="button" className="btn btn-outline-success" onClick={() => props.plusMinus(props.index, 3)}>+3</button>
+      <button type="button" className="btn btn-outline-success" onClick={() => props.plusMinus(props.index, 2)}>+2</button>
+      <button type="button" className="btn btn-outline-success" onClick={() => props.plusMinus(props.index, 1)}>+1</button>
 
     </div>
   );
